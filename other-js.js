65fe77ebd0266e8ee87f8b896785f959adccaaec
Ron <ronocheri@gmail.com>
2018-05-24 11:20:50 +0300

    24-5

--- other-js.js
+++ other-js.js
@@ -76,10 +76,10 @@ function loginLocalStorage(){
 
     //picture in profile page
 
-    x.setAttribute("src", "http://localhost/gameonphp/upload/"+user_image);
+    x.setAttribute("src", "http://35.233.58.223/gameonphp/upload/"+user_image);
 
     //picture in profile page
-    y.setAttribute("src", "http://localhost/gameonphp/upload/"+user_image);
+    y.setAttribute("src", "http://35.233.58.223/gameonphp/upload/"+user_image);
 
 
   }
@@ -89,10 +89,10 @@ function loginLocalStorage(){
 
     //picture in profile page
 
-    x.setAttribute("src", "http://localhost/gameonphp/upload/user.png");
+    x.setAttribute("src", "http://35.233.58.223/gameonphp/upload/user.png");
 
     //picture in profile page
-    y.setAttribute("src", "http://localhost/gameonphp/upload/user.png");
+    y.setAttribute("src", "http://35.233.58.223/gameonphp/upload/user.png");
 
   }
   //go to main menu
@@ -355,7 +355,7 @@ var deviceReady = false;
 
        // Verify server has been entered
 
-       server = "http://localhost/gameonphp/upload.php";
+       server = "http://35.233.58.223/gameonphp/upload.php";
 
        if (server) {
 
